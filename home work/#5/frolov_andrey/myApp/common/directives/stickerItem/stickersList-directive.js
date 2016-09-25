7d8e367b463e047f189e0f917e6c751903b6487d
Pouin <macfrolov@yandex.ru>
2016-09-26 00:48:31 +0300

    add filter

--- home work/#5/frolov_andrey/myApp/common/directives/stickerItem/stickersList-directive.js
+++ home work/#5/frolov_andrey/myApp/common/directives/stickerItem/stickersList-directive.js
@@ -1,11 +1,12 @@
 (function () {
     'use strict';
 
+
     angular
         .module('myApp')
-        .directive('stickersListDirective', stickerList);
-
-
+        .directive('stickersListDirective', stickerList)
+        .filter('priceFil', ['$sce', priceFilter])
+        .filter('orderFil', ['$filter', '$sce', orderFil]);
 
     function stickerList() {
         var directive = {
@@ -13,11 +14,31 @@
             templateUrl: 'myApp/common/directives/stickerItem/stickersList.html',
             replace: true,
             scope: {
-                 stickers: '='
+                stickers: '='
             }
         };
 
         return directive;
+    }
+
+    function orderFil($filter) {
+        return function (item) {
+            return $filter('orderBy')(item);
+        };
+    }
+
+    function priceFilter($sce) {
+        return function (item) {
+            if (item <= '10') {
+                return item = $sce.trustAsHtml("<span style='color: black; font-size: large'>" + item + "</span>");
+            }
+            else if (item > '10' && item <= '20') {
+                return item = $sce.trustAsHtml("<span style='color: orange; font-size: large'>" + item + "</span>");
+            }
+            else {
+                return item = $sce.trustAsHtml("<span style='color: red; font-size: large'>" + item + "</span>");
+            }
+        };
     }
 
 })();
